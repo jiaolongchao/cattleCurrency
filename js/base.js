8018ef8ff15338d9b904a4f408b2959d8732fdf9
jiaolongchao <jiaolongchao@9086.cn>
2017-09-27 14:20:11 +0800

    cattleCurrency

---
+++ js/base.js
@@ -0,0 +1,383 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = clientWidth / 7.5 + 'px';
+        };
+    // Abort if browser does not support addEventListener
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+var httpApi="http://currency-dev.0606.com.cn/";
+var httpApi2 = 'http://advisor-dev.0606.com.cn:5000/';
+var httpApi3 = 'http://123.56.30.141:9600/';
+
+//var Http='http://101.201.237.183:5700/
+//生产环境
+//var Http = 'http://101.201.237.183:8000/';
+//测试环境
+var Http='http://buss.0606.com.cn:8080/';
+//正式环境
+var accessToken = 'a4c05812bb817cf2f53ebc25a9846b36';
+$(function() {
+    if (typeof window.webkit != 'undefined') {
+        window.webkit.messageHandlers.jsCallNative.postMessage({"nativeCallJS":"refreshtoken"});
+    }else if(/Android/i.test(window.navigator.userAgent)) {				       //android
+        window.haina.pushEvent('{"nativecalljs":"refreshtoken"}');
+    }
+})
+
+/*function refreshtoken(token){
+    accessToken =  token;
+}*/
+
+
+function getTotal(){  //获取牛币总数
+    $.ajax({
+        type: "get",
+        url: httpApi + 'currency/info.jspa?access_token=' + accessToken,
+        dataType: 'json',
+        success: function (datas) {
+            if(datas.code == 200){
+                var data = datas.data;
+                $('.HeadNum').html(data.total + '<small>个</small>'); //牛币总数
+                $('.HeadNum').html(data.inviteReAmount + data.inviteBuyAmount + '<small>个</small>');
+                $('.inviteFirend').html(data.inviteReTimes);
+                $('.invitetake').html(data.inviteBuyTimes);
+
+            }else{
+                console.log(' ')
+            }
+        },
+        error: function (e) {
+            console.log("error")
+        }
+    })
+}
+
+function getUrlParam(name) {  //获取url的code
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+function getRecord(filtter,page,pageSize) {
+    $.ajax({  //牛币记录
+        type: "get",
+        url: httpApi + 'currency/record.jspa?access_token=' + accessToken + '&type='+ filtter +'&pageNo='+ page +'&pageSize='+ pageSize ,
+        dataType: 'json',
+        success: function (datas) {
+            console.log(datas)
+            if(datas.code == 200){
+                var data = datas.data.records;
+                /*var data = [{amounts: "-50", remark: "descdescdesc", time: '1506139931'},
+                 {amounts: "+1000", remark: "好友购买智选组合5号获得", time: '1506139931'},
+                 {amounts: "+1000", remark: "好友购买智选组合2号获得", time: '1506139931'},
+                 {amounts: "+500", remark: "购买智选组合2号获得", time: '1507139931'},
+                 {amounts: "+5", remark: "购买智选组合2号获得", time: '1507139931'},
+                 {amounts: "+2", remark: "购买智选组合一号获得", time: '21507139931'},
+                 {amounts: "+500", remark: "注册奖励", time: '21508139931'},
+                 {amounts: "+5", remark: "购买智选组合2号获得", time: '21508139931'},
+                 {amounts: "+2", remark: "购买智选组合一号获得", time: '21508139931'},
+                 {amounts: "+500", remark: "注册奖励", time: '21508139931'}]*/
+                /*var newary = [];
+                function filterAry(data){
+                    var temp = [];
+                    if(data.length == 0) return false;
+                    for(var i = 0; i < data.length; i++){
+                        temp.push(data[i])
+                        if(timestamp(data[i].time,'-') == timestamp(data[data.length - 1].time,'-')) return;  //保留数组中最后一项
+                        if(timestamp(data[i].time,'-') != timestamp(data[i+1].time,'-')) break;
+                    }
+                    newary.push(temp);
+                    data.splice(0,temp.length);  //删除已经过滤的项
+                    filterAry(data);
+                }
+                filterAry(data)
+                newary.push(data)*/
+
+                function o(data) {
+                    let obj={};
+                    data.map((item)=>{
+                        if(!obj[ timestamp(item.time,'-') ]){
+                            obj[ timestamp(item.time,'-') ]=[];
+                        }
+                        obj[ timestamp(item.time,'-') ].push(item)
+                    })
+                    let ary=[];
+                    for(let k in obj){
+                        ary.push(obj[k])
+                    }
+                    return ary;
+                }
+                var newary = o(data);
+
+                var html = '';
+                var oPart = '';
+                for(var i = 0;i < newary.length;i++){
+                    var oLi = '';
+                    for(var j = 0; j < newary[i].length;j++){
+                        oLi +=  '<li><div class="inviteTop"><span>'+ timestamp(newary[i][j].time,'ch') +'</span><span>'+ newary[i][j].amounts +'牛币</span></div><span class="inviteBot">'+ newary[i][j].remark +'</span></li>'
+                        oPart = '<div class="dataTime">'+ timestamp(newary[i][0].time,'-') +'</div><ul class="inviteRecord">' +  oLi + '</ul>';
+                    }
+                    html += oPart;
+                }
+                $('.recordList').html(html);
+            }else{
+                console.log(datas.message)
+            }
+        },
+        error: function (e) {
+            console.log("error")
+        }
+    })
+}
+
+function colsedpop (obj){  //关闭pop
+    $(obj).parents('.pop_box').hide()
+}
+//时间戳转换成  年-月-日
+function timestamp(date,separator){
+    if(date===""){
+        return "--";
+    }else{
+        var date =new Date(date*1000);
+        var Y = date.getFullYear();
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
+        var D = date.getDate();
+        var h=(date.getHours()<10 ? "0"+(date.getHours()) : date.getHours())+':';
+        var m=(date.getMinutes()<10 ? "0"+(date.getMinutes()) : date.getMinutes())+':';
+        var s=(date.getSeconds()<10 ? "0"+(date.getSeconds()) : date.getSeconds());
+        if(D<10){
+            D=0+D;
+        }
+        if(separator == '-'){
+            return ( Y+ '-' + M);
+        }else if(separator == 'ch'){
+            return ( Y+ '年' + M + '月' + D + '日');
+        }else{
+            return ( Y + M + D);
+        }
+
+    }
+}
+
+function check_name() {
+    var phone = document.getElementById("phone").value;
+    if (phone) {
+        if (!check_phone(phone)) {
+            //showError("请输入正确的手机号！");
+            layer.open({
+                btn: ['<span class="single">确定</span>'],
+                shadeClose: false,
+                content: '请输入正确的手机号'
+            });
+            return false;
+        }
+    } else {
+        //showError("请输入手机号！");
+        layer.open({
+            btn: ['<span class="single">确定</span>'],
+            shadeClose: false,
+            content: '请输入手机号'
+        });
+        document.getElementById("phone").focus(); //手机号input获得焦点
+        return false;
+    }
+    return true;
+}
+
+function check_phone_keyup(e){ /*手机号非字符验证*/
+    var e = $(this);
+    e.val(e.val().replace(/\s|[^\d]/g,''));
+}
+
+function check_phone(phone) {  /*手机号码验证*/
+    var reg = /^0?(13[0-9]|15[0-9]|17[0-9]|18[0-9]|14[57])[0-9]{8}$/;
+    if (!reg.test(phone)) {
+        return false;
+    }
+    return true;
+}
+
+function showError(msg) {  //错误提示
+    var showDiv = $('.error_timeout')[0], bgDiv = $('.mask')[0];
+    var wHeight = $(window).height();
+    var showDivHeighr = $(showDiv).height();
+    $(showDiv).css('top', (wHeight - showDivHeighr) / 2);
+    $(bgDiv).show();
+    $(showDiv).html(msg).show();
+    window.setTimeout(function(){
+        $(bgDiv).hide();
+        $(showDiv).hide();
+    },1000)
+}
+
+//倒计时功能  按钮ID，秒+1，显示文字
+function _clocking(id,i){
+    var btn = document.getElementById(id);
+    btn.setAttribute("onclick","");
+    btn.style.backgroundColor="#fff";
+    btn.style.borderColor="#999999";
+    btn.style.color="#999999";
+    document.getElementById("phonetext").innerHTML="";
+    _timeSet(id,i);
+}
+function _timeSet(id,i){
+    i--;
+    if(i==0){
+        document.getElementById("phoneCodeSent").innerHTML="";
+        document.getElementById("phonetext").innerHTML="重新获取";
+        document.getElementById(id).setAttribute("onclick","getRecode()");
+        document.getElementById(id).style.backgroundColor="#fff";
+        document.getElementById(id).style.borderColor="#FF5A1C";
+        document.getElementById(id).style.color="#FF5A1C";
+    }
+    else{
+        document.getElementById("phoneCodeSent").innerHTML = i+ "s重新获取";
+        setTimeout("_timeSet(\""+id+"\","+i+")",1000);
+    }
+}
+function getRecode(){
+    if(!check_name()) return;
+    var client_id = getUrlParam('_id');
+    $.ajax({
+        type: "get",
+        url : httpApi3 + 'api/v2/security/authorize?response_type=base&client_id='+ client_id,
+        dataType: 'json',
+        success: function (datas) {
+            if(datas.code == 200){
+                var data = datas.data;
+                console.log(data.auth_code)
+                var auth_code = data.auth_code;
+                var auth_message = {
+                    "mobile": $('#phone').val(),
+                    "auth_code":data.auth_code,
+                    "type":1
+                }
+                auth_message = JSON.stringify(auth_message);
+                $.ajax({
+                    type: "post",
+                    url : httpApi3 + 'api/v2/security/sms/send?response_type=base&client_id=' + client_id,
+                    dataType: 'json',
+                    data: auth_message,
+                    success: function (datas) {
+                        console.log(datas)
+                        if(datas.code == 200){
+                            console.log('获取验证码成功')
+                            _clocking("getRecode",60);
+                        }else if(datas.code == 41015){
+                            layer.open({
+                                btn: ['<span class="single">确定</span>'],
+                                shadeClose: false,
+                                content: '手机验证码错误'
+                            });
+                        }else if(datas.code == 41016){
+                            layer.open({
+                                btn: ['<span class="single">确定</span>'],
+                                shadeClose: false,
+                                content: '手机号码已经被绑定'
+                            });
+                        }else if(datas.code == 41018){
+                            layer.open({
+                                btn: ['<span class="single">确定</span>'],
+                                shadeClose: false,
+                                content: '验证码过期'
+                            });
+                        }else{
+                            console.log('获取验证码失败');
+                        }
+                    },
+                    error: function (e) {
+                        console.log("error")
+                    }
+                })
+
+            }else{
+                console.log('获取授权码失败')
+            }
+        },
+        error: function (e) {
+            console.log("error")
+        }
+    })
+}
+
+function check_phone_keyup(e){ //正则验证手机号输入
+    var e = $(e);
+    e.val(e.val().replace(/\s|[^\d]/g,''));
+}
+
+//		更新下载次数
+function downloadCont(source,channel){
+    //1:IOS,2android
+    $.ajax({
+        type:"post",
+        url: Http + 'StatisticsLog/addLog ',
+        dataType:'json',
+        data:'{"Source":'+source+',"Channel":"'+channel+'","Download":1}',
+        success:function(datas){
+            if(datas.code==200){
+                console.log("更新下载次数");
+            }else{
+                console.log("数据返回错误乱码");
+            }
+        },
+        error:function(e){
+            console.log("error")
+        }
+    });
+}
+
+function isWeiXin(){
+    var ua = window.navigator.userAgent.toLowerCase();
+    if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//判断ios系统版本号
+function iosEdition(){
+    var ver = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    ver = parseInt(ver[1], 10);
+    return ver;
+}
+
+//调用分享
+
+//分享
+function share(){
+    var shareValue = {
+        "title": "超级战队：三步捕获牛股 金牛回头战法4天获利31.5%!",
+        "desc": "操盘大师战队利用金牛回头战法捕获澄天伟业、元隆雅图等牛股，4天获利超过3成!",
+        "imageUrl": HttpLink + "images/zjzd.jpg",
+        "url": window.location.href + '&from=share',
+        "site": "海纳牛牛",
+        "siteUrl": window.location.href + '&from=share',
+        "titleUrl": window.location.href + '&from=share',
+        "shareType": "all"
+    };
+    if(/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){
+        console.log("ios")
+        //ios
+        window.webkit.messageHandlers.jsCallNative.postMessage({"nativeCallJS":"share","sharevalue":shareValue});
+    }else {
+        //android
+        var value= JSON.stringify(shareValue);
+        window.haina.pushEvent('{"nativecalljs":"share", "sharevalue":'+value+'}');
+    }
+
+}
+
+
+
+
+
+
+
